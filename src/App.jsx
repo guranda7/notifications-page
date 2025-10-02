16fb39af7f307849693af4eaf30ff0b8bee1a04c
guranda <gurandaggg@gmail.com>
2025-10-02 20:30:19 +0400

    create notification component

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,15 @@
 import { useState } from 'react'
 import './App.css';
-import image1 from "./assets/images/avatar-mark-webber.webp"
+import image1 from "./assets/images/avatar-mark-webber.webp";
+import data from "./data.json";
+import Notification from './components/Notification';
 
 function App() {
-  
+  data.map((item) => console.log(item))
 
   return (
     <>
-      <div className="main-container">
-        <section className="header">
-          <div className="header-title">
-            <span>Notifications</span>
-            <div className="not-counter"><span>3</span></div>
-          </div>
-          <p className="mark-title">Mark all as read</p>
-        </section>
-
-        <section className="notifications-container">
-          <div className="notification">
-            <img src={image1} alt="image" />
-            <p><span className="name">Mark Webber</span> <span className="description">reacted to your recent post</span> <span>My first tournament today!</span> </p>
-          </div>
-        </section>
-      </div>
+      <Notification image1={image1}/>
     </>
   )
 }
---
+++ src/components/Notification.jsx
@@ -0,0 +1,22 @@
+export default function Notification({image1}) {
+    return (
+        <div>
+            <div className="main-container">
+                    <section className="header">
+                      <div className="header-title">
+                        <span>Notifications</span>
+                        <div className="not-counter"><span>3</span></div>
+                      </div>
+                      <p className="mark-title">Mark all as read</p>
+                    </section>
+            
+                    <section className="notifications-container">
+                      <div className="notification">
+                        <img src={image1} alt="image" />
+                        <p><span className="name">Mark Webber</span> <span className="description">reacted to your recent post</span> <span>My first tournament today!</span> </p>
+                      </div>
+                    </section>
+                  </div>
+        </div>
+    )
+}
