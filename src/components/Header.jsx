04309a831ca33a622c838ff50d8d8416b6ccd107
guranda <gurandaggg@gmail.com>
2025-10-03 17:04:51 +0400

    create onclick functions  and change styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,42 @@
+import { useState } from "react";
 import "./Header.css"
 import Notification from "./Notification";
 import data from "../data.json";
 
-
 export default function Header() {
+    const [notAmount, setNotAmount] = useState(data.length); 
+    const [isClicked, setIsClicked] = useState(false);
+    function decreaseCount() {
+        setNotAmount((prev) => prev - 1)
+    }
+
+    function handleClick() {
+        setIsClicked(true)
+    }
+    
     return (
         <div>
             <div className="main-container">
                     <section className="header">
                       <div className="header-title">
                         <span>Notifications</span>
-                        <div className="not-counter"><span>3</span></div>
+                        {notAmount > 0 ?  <div className="not-counter"><span>{notAmount}</span></div> : ""}
+                       
                       </div>
                       <p className="mark-title">Mark all as read</p>
                     </section>
                     {
                         data.map((item) => (
-                            <Notification image1={item.image} name={item.name} description={item.description} post={item.post} key={item.id} time={item.time}/>
+                            <Notification 
+                            image1={item.image}
+                             name={item.name} 
+                             description={item.description} 
+                             post={item.post} 
+                             key={item.id} 
+                             time={item.time}
+                             onClickDescription={decreaseCount}
+                             handleClick={handleClick}
+                             isClicked={isClicked}/>
                         ))
                     }   
             </div>
--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,17 +1,18 @@
 import "./Notification.css"
 
-export default function Notification({image1, name, description, post, time}) {
+export default function Notification({image1, name, description, post, time, onClickDescription, handleClick, isClicked }) {
     return (
         <div>
            
                     <section className="notifications-container">
-                      <div className="notification">
+                      <div className="notification" style={{backgroundColor: isClicked ? "white" : ""}}>
                         <img src={image1} alt="image" className="not-image"/>
-                        <div className="description">
-                            <p><span className="name">{name}</span><span className="description">{description}</span> <span>{post}</span> </p>
-                            
+                        <div className="description" onClick={() => {onClickDescription(); handleClick() }}>
+                            <div className="not-description"><span className="name">{name}</span><span className="span-description">{description}</span> <span className="not-post">{post}</span> <div className="red-circle"></div>
+                            </div>
+                            <span className="not-time">{time}</span>
                         </div>
-                        <br/><span>{time}</span>
+                        
                       </div>
                     </section>
                  
